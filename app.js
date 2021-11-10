26f18d0e438a1bef19439592a26d9cf412f47679
Karina Freitas <karinaglf@gmail.com>
2021-11-10 10:05:55 +0000

    add 3rd project

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const PORT = 3000;
 
 const app = express();
 
@@ -7,17 +8,25 @@ const app = express();
 app.use(express.static("public"));
 
 //GET
-//HOMEPAGE
+//PAGES
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/views/index.html");
 });
 
-// 404 ERRORS
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/public/views/about.html");
+});
+
+app.get("/works", (req, res) => {
+  res.sendFile(__dirname + "/public/views/works.html");
+});
+
+// 404 ERROR
 app.use(function (req, res, next) {
   return res.status(404).send({ message: "Route" + req.url + " Not found." });
 });
 
 
-app.listen(3000, () => {
-  console.log("Server is running");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
